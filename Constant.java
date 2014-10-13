ee80b0e50992f19c8afb52e119dec20e66f30e3b
abhishek2521 <akapoor@buffalo.edu>
2014-10-13 15:28:41 -0400

    Create Constant.java

---
+++ Constant.java
@@ -0,0 +1,20 @@
+
+package edu.buffalo.cse.cse486586.simpledht;
+
+public class Constants {
+
+    public static String JOINPROTOCOL = "JOIN";
+    public static String REASSIGNSUCCESSOR = "REASSIGNSUCCESSOR";
+    public static String REASSIGNPREDECESSOR = "REASSIGNPREDECESSOR";
+    public static String INSERTPROTOCOL = "INSERT";
+    public static String PROPAGATEINSERT = "PROPAGATEINSERT";
+    public static String DELETEPROTOCOL = "DELETE";
+    public static String PROPAGATEDELETE = "PROPAGATEDELETE";
+    public static String BROADCASTDELETE = "BROADCASTDELETE";
+    public static String DELETERESPONSE = "DELETERESPONSE";
+    public static String QUERYPROTOCOL = "QUERY";
+    public static String PROPAGATEQUERY = "PROPAGATEQUERY";
+    public static String BROADCASTQUERY = "BROADCASTQUERY";
+    public static String QUERYRESPONSE = "QUERYRESPONSE";
+
+}
